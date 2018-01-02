f92c7f59b36b40ceac1171c9f3ab0ca6a293b04a
Khauê Viana <khaueviana@gmail.com>
2018-01-02 11:41:25 -0200

    Add Native HTTP request

---
+++ https-native.js
@@ -0,0 +1,18 @@
+const https = require("https");
+const url = "https://maps.googleapis.com/maps/api/geocode/json?address=Florence";
+
+https.get(url, res => {
+    res.setEncoding("utf8");
+    let body = "";
+    res.on("data", data => {
+        body += data;
+    });
+    res.on("end", () => {
+        body = JSON.parse(body);
+        console.log(
+            `City: ${body.results[0].formatted_address} -`,
+            `Latitude: ${body.results[0].geometry.location.lat} -`,
+            `Longitude: ${body.results[0].geometry.location.lng}`
+        );
+    });
+});
